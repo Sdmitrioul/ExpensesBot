a70cf55176202b3f69a197bd9dc0bc914fd9d76e
Dmitrii Skroba <dmitri.zd1958@gmail.com>
2024-11-21 19:11:44 +0000

    notion first classes

---
+++ src/main/java/com/dskroba/notion/NotionException.java
@@ -0,0 +1,17 @@
+package com.dskroba.notion;
+
+import com.dskroba.base.exception.CustomException;
+
+public class NotionException extends CustomException {
+    public NotionException(String message) {
+        super(message);
+    }
+
+    public NotionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotionException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/dskroba/notion/NotionHeadersProvider.java
@@ -0,0 +1,38 @@
+package com.dskroba.notion;
+
+public class NotionHeadersProvider {
+    private final String token;
+    private final String version;
+
+    public NotionHeadersProvider(String token, String version) {
+        this.token = token;
+        this.version = version;
+    }
+
+    public String[] getGetHeader() {
+        return getHeader();
+    }
+
+    public String[] getPostHeader() {
+        return getHeader("Content-Type", "application/json");
+    }
+
+    private String[] getHeader(String ... additionalHeaders) {
+        int size = (additionalHeaders == null ? 0 : additionalHeaders.length) + 6;
+        String[] headers = new String[size];
+        headers[0] = "Authorization";
+        headers[1] = token;
+        headers[2] = "Notion-Version";
+        headers[3] = version;
+        headers[4] = "Accept";
+        headers[5] = "application/json";
+        if (additionalHeaders == null) {
+            return headers;
+        }
+        int i = 6;
+        for (String header : additionalHeaders) {
+            headers[i++] = header;
+        }
+        return headers;
+    }
+}
